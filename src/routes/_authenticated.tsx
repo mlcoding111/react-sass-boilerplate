6e2332c9b63c5a90b14013bef5532e8dd3ce1df0
Michael Lacroix <kennycodes@hotmail.com>
2025-10-14 23:14:56 -0400

    remove redirect condition

--- src/routes/_authenticated.tsx
+++ src/routes/_authenticated.tsx
@@ -2,11 +2,11 @@ import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_authenticated')({
   component: RouteComponent,
-  beforeLoad: async ({ context }) => {
-    if (!context.isAuthenticated) {
-      throw redirect({ to: "/login" });
-    }
-  },
+  // beforeLoad: async ({ context }) => {
+  //   if (!context.isAuthenticated) {
+  //     throw redirect({ to: "/login" });
+  //   }
+  // },
 })
 
 function RouteComponent() {
